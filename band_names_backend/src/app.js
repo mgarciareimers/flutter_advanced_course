b900f1b48194efc5f76544afce8d1f543eba2ab5
Miguel García Reimers <mgarciareimers@gmail.com>
2021-04-15 20:29:19 +0200

    band_names_backend: Express static implemented.

--- band_names_backend/src/app.js
+++ band_names_backend/src/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 
+// Initialize app.
 const app = express();
+
+// Public path.
+const publicPath = path.resolve(__dirname, '../public');
+
+// Use static content.
+app.use(express.static(publicPath));
 
 app.listen(3000, (error) => {
     if (error) {
