b918fc4ed41117f4e57cfd6b4a8faf9737fc1315
Miguel García Reimers <mgarciareimers@gmail.com>
2021-04-16 09:55:35 +0200

    band_names_backend: Environment variables added.

--- band_names_backend/src/app.js
+++ band_names_backend/src/app.js
@@ -16,5 +16,5 @@ app.listen(process.env.PORT, (error) => {
         throw new Error(error);
     }
 
-    console.log('Server running on PORT', process.env.PORT);
+    console.log('Server running on port', process.env.PORT);
 });
